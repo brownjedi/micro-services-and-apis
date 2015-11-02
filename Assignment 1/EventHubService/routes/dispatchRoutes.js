395b11846586941b9302e2ce37e6c033c359e7a9
Sai Karthik Reddy Ginni <saikarthikreddyginni@outlook.com>
2015-11-02 12:58:57 -0500

    EventHubService: Changes to the dispatchRoutes

--- Assignment 1/EventHubService/routes/dispatchRoutes.js
+++ Assignment 1/EventHubService/routes/dispatchRoutes.js
@@ -8,28 +8,52 @@ const Subscription = require('./../models/subscription');
 const dataTransformer = require('./../utilities/dataTransformer');
 
 router.post('/', (req, res) => {
-	// Check if the user sent the event in the request body.
-	// If not, return 400 Bad request error.
+
+    /*
+    {
+        type: "STUDENT_UPDATED_ERROR",
+        version: "ver",
+        data: {
+            studentID:
+            courses: [2]
+        }
+    }
+
+    1. Check if all courses are present and valid....
+    2. then iterate through courses and add student
+
+    {
+        type: "STUDENT_DELETED",
+        version: "ver",
+        data: {
+            studentID
+        }
+    }
+
+    */
+
+    // Check if the user sent the event in the request body.
+    // If not, return 400 Bad request error.
     if (!req.body || !req.body.type) {
         return res.status(400).json(dataTransformer.transformError(400, "Bad Request, Request body is null. The POST method must have the data(JSON) in the Request body"));
     }
 
     // Get the list of subscriptions from the database and dispatch the event.
     Subscription.find({}, (err, subscriptions) => {
-    	if(err) {
-    		return res.status(400).json(dataTransformer.transformError(err.status, err.message));
-    	} else {
-    		res.status(204).end();
-    	}
+        if (err) {
+            return res.status(400).json(dataTransformer.transformError(err.status, err.message));
+        } else {
+            res.status(204).end();
+        }
 
-    	subscriptions.forEach((subscription) => {
-    		if(subscription.events.indexOf(req.body.type.toLowerCase()) > -1) {
-				request
-					.post(subscription.callback)
-					.set('Content-Type', 'application/json')
-					.end();
-    		}
-    	});
+        subscriptions.forEach((subscription) => {
+            if (subscription.events.indexOf(req.body.type.toLowerCase()) > -1) {
+                request
+                    .post(subscription.callback)
+                    .set('Content-Type', 'application/json')
+                    .end();
+            }
+        });
     });
 });
 
