ad6884918f40d445f630a9dcc9e8973ffb9d833e
Sai Karthik Reddy Ginni <sg3205@columbia.edu>
2015-11-25 23:02:56 -0500

    StudentService: modified the error handler

--- Assignment 1/StudentService/server.js
+++ Assignment 1/StudentService/server.js
@@ -79,7 +79,7 @@ app.use((req, res, next) => {
 });
 
 // Error Handler
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
     res.status(err.status || 500);
     return res.sendData(util.generateErrorJSON(err.status, err.message));
 });
